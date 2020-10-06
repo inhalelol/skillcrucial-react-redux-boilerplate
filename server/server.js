e6c1107b581aaf6c906814489ca8323ac41e98d7
inhalelol <medvedev.sasha1998@gmail.com>
2020-10-06 13:35:38 +0300

    task 1 on 12 week

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-import express from 'express'
+import express, { response } from 'express'
 import path from 'path'
 import axios from 'axios'
-// import fs from 'fs.promises'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
@@ -11,7 +10,7 @@ import cookieParser from 'cookie-parser'
 import config from './config'
 import Html from '../client/html'
 
-const { readFile, writeFile} = require("fs").promises;
+const { readFile, writeFile, unlink } = require("fs").promises;
 
 const Root = () => ''
 
@@ -34,53 +33,80 @@ let connections = []
 const port = process.env.PORT || 8090
 const server = express()
 
+const setHeader = (req, res, next) => {
+  res.set('x-skillcrucial-user', '5f1a172f-fef4-48a6-954b-50e9f266eabc')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  next()
+}
+
 const middleware = [
   cors(),
   express.static(path.resolve(__dirname, '../dist/assets')),
   bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
   bodyParser.json({ limit: '50mb', extended: true }),
-  cookieParser()
+  cookieParser(),
+  setHeader
 ]
 
 middleware.forEach((it) => server.use(it))
 
-function toWriteFile (dataFile){
+function toWriteFile(dataFile){
   writeFile(`${__dirname}/users.json`, (JSON.stringify(dataFile)), { encoding: "utf8" }) 
 }
 function fileContent(){
-  const bigData = readFile(`${__dirname}/users.json`, { encoding: "utf8" })
-  .then(users => {  
+  const bigData = readFile(`${__dirname}/users.json`)
+  .then((file) => {  
     /* вернется текст из файла, а не объект джаваскрипта */
-    return JSON.parse(users)  
-  })  
+    return JSON.parse(file)  
+  })
   .catch(async () => {  
     /* случается когда нет файла */
     const result = await axios('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.data)
+    response.sort((a, b) => a.id - b.id)
     toWriteFile(result.data)
     return result.data
   })
   return bigData
 }
 
-server.get('/api/v2/users', async (req, res) => {
-  const newData = await fileContent()
-  res.send(newData)
-})
-
 server.get('/api/v1/users', async (req, res) => {
-  const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
-  res.json(users)
+  const newData = await fileContent()
+  res.json(newData)
 })
 
-server.get('/api/v1/users/take/:number', async (req, res) => {
-  const { number } = req.params
-  const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
-  res.json(users.slice(0, +number))
+server.post('/api/v1/users', async (req, res) => {
+  const newUser = req.body
+  const userData = await fileContent()
+  newUser.id = (userData.length === 0) ? 1 : userData[userData.length - 1].id + 1
+  toWriteFile([...userData, newUser])
+  res.json({ status: 'succes', id: newUser.id })
 })
 
-server.get('/api/v1/users/:name', (req, res) => {
-  const { name } = req.params
-  res.json({ name })
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const { userId } = req.params
+  const newUser = req.body
+  const arr = await fileContent()
+  const objId = arr.find((obj) => obj.id === +userId)
+  const objId2 = { ...objId, ...newUser }
+  const arr2 = arr.map((rec) => rec.id === objId2.id ? objId2 : rec )
+  toWriteFile(arr2)
+  res.json({ status: 'succes', id: userId })
+})
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const { userId } = req.params
+  const arr = await fileContent()
+  const objId = arr.filter((obj) => obj.id === +userId)
+  const arr2 = arr.map((rec) => rec.id !== objId.id)
+  toWriteFile(arr2)
+  res.json({ status: 'succes', id: userId })
+})
+
+server.delete('/api/v1/users/', (req, res) => {
+  unlink(`${__dirname}/users.json`)
+    .then(() => res.json({ status: 'succes' }))
+    .catch(() => res.send('file not find'))
 })
 
 server.use('/api/', (req, res) => {
